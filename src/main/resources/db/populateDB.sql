bf237ee4629863efa507341e5f9c5f8aeb522ad4
Alex Klokov <cvbn760@yandex.ru>
2020-04-02 06:27:52 +0300

    2 Дополнить скрипты создания и инициализации базы таблицой MEALS. Запустить скрипты на вашу базу (через Run). Порядок таблиц при DROP и DELETE важен, если они связаны внешними ключами (foreign key, fk). Проверьте, что ваши скрипты работают

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,23 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+('User', 'user@yandex.ru', 'password'),
+('Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+('ROLE_USER', 100000),
+('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (userId, datetime, description, calories) VALUES
+(100000,'2020-01-30 10:00', 'Завтрак', 500),
+(100000,'2020-01-30 14:00', 'Обед', 1000),
+(100000,'2020-01-30 19:00', 'Ужин', 500),
+(100000,'2020-01-30 00:00', 'Еда на граничное значение', 100),
+(100000,'2020-01-31 10:00', 'Завтрак', 1000),
+(100000,'2020-01-31 14:00', 'Обед', 500),
+(100000,'2020-01-31 19:00', 'Ужин', 410),
+(100001,'2020-01-30 11:00', 'Админ ланч', 510),
+(100001,'2020-01-30 20:00', 'Админ ужин', 1500);
