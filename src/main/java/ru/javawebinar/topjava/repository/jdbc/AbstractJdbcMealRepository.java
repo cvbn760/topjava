1caff7aedabed2cbed5305eaee31d4d5f4eb9784
Alex Klokov <cvbn760@yandex.ru>
2020-04-26 13:15:18 +0300

    FIX |
    - починить SpringMain
    - разделить JdbcMealRepository для HSQLDB и Postgres

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/AbstractJdbcMealRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.javawebinar.topjava.repository.jdbc;
-
-import java.time.LocalDateTime;
-
-public abstract class AbstractJdbcMealRepository {
-
-    abstract String dateConversion(LocalDateTime dateTime);
-}
